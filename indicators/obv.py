edbce13c0abe241237d4112d98d456a8f95e338b
admin <admin@admins-MacBook-Air.local>
2020-03-30 08:43:24 -0400

    Added new indicators, kpis, and backtesting

---
+++ indicators/obv.py
@@ -0,0 +1,20 @@
+# Import necesary libraries
+import pandas_datareader.data as pdr
+import numpy as np
+import datetime
+
+# Download historical data for required stocks
+ticker = "AAPL"
+ohlcv = pdr.get_data_yahoo(ticker, datetime.date.today(
+)-datetime.timedelta(364), datetime.date.today())
+
+
+def OBV(DF):
+    """function to calculate On Balance Volume"""
+    df = DF.copy()
+    df['daily_ret'] = df['Adj Close'].pct_change()
+    df['direction'] = np.where(df['daily_ret'] >= 0, 1, -1)
+    df['direction'][0] = 0
+    df['vol_adj'] = df['Volume'] * df['direction']
+    df['obv'] = df['vol_adj'].cumsum()
+    return df['obv']
